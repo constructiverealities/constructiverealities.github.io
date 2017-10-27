ef714f82cababc9c07309fc2a857ce346e926691
Eric Schleicher <eric.schleicher@gmail.com>
2017-10-27 09:20:00 -0700

    Catchup commmit.   I guess a few new resources added.

---
+++ scrap.js
@@ -0,0 +1,372 @@
+var x = {
+    "status": {
+        "code": 0, "processing": "SUCCEEDED", "warning": {}
+        ,
+        "error": {}
+    }
+    ,
+    "uid": "67d4415608c1474eac0298bfda83a079",
+    "publishedAt": null,
+    "likeCount": 0,
+    "commentCount": 0,
+    "archiveSize": 15975354,
+    "isDownloadAllowed": true,
+    "isDeleted": false,
+    "viewCount": 0,
+    "processingStatus": 2,
+    "staffpickedAt": null,
+    "isDownloadable": false,
+    "isRestricted": false,
+    "isPrivate": false,
+    "embedUrl": "https:\/\/sketchfab.com\/models\/67d4415608c1474eac0298bfda83a079\/embed",
+    "files": [{
+        "wireframeSize": 2213356,
+        "uid": "3ec7a5eb2c0b46fc9034306ef0c92f24",
+        "osgjsUrl": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/urls\/67d4415608c1474eac0298bfda83a079\/dist\/models\/3ec7a5eb2c0b46fc9034306ef0c92f24\/file.osgjs.gz",
+        "flag": 0,
+        "modelSize": 3438156,
+        "osgjsSize": 29514
+    }],
+    "description": "",
+    "tags": [],
+    "viewerUrl": "https:\/\/sketchfab.com\/models\/67d4415608c1474eac0298bfda83a079",
+    "isProtected": false,
+    "categories": [],
+    "vertexCount": 128787,
+    "user": {
+        "username": "eric.schleicher",
+        "account": "free",
+        "displayName": "eric.schleicher",
+        "uid": "09b025e0b9e54dd0bb3bbc2838db325f",
+        "avatars": {
+            "images": [{
+                "url": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/avatars\/4ca51acb67804c318c40e0e2c205fc14\/6d56445e90ad4873b282f8f2ed9edf33.jpeg",
+                "width": 32,
+                "height": 32,
+                "size": 807
+            }, {
+                "url": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/avatars\/4ca51acb67804c318c40e0e2c205fc14\/e4bd46e3f99544e0bfc69d73c8072e37.jpeg",
+                "width": 48,
+                "height": 48,
+                "size": 1221
+            }, {
+                "url": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/avatars\/4ca51acb67804c318c40e0e2c205fc14\/5b260567ed6f481da5ae50414eab8104.jpeg",
+                "width": 90,
+                "height": 90,
+                "size": 2957
+            }, {
+                "url": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/avatars\/4ca51acb67804c318c40e0e2c205fc14\/37961ec0e79c4de68e80b5007ba39415.jpeg",
+                "width": 100,
+                "height": 100,
+                "size": 3378
+            }], "uid": "4ca51acb67804c318c40e0e2c205fc14"
+        }
+        ,
+        "profileUrl": "https:\/\/sketchfab.com\/eric.schleicher"
+    }
+    ,
+    "originalFileName": "chair",
+    "thumbnails": {
+        "images": [{
+            "url": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/urls\/67d4415608c1474eac0298bfda83a079\/dist\/thumbnails\/29796177d94c4f03b5b189515a14233c\/1024x576.jpeg",
+            "width": 1024,
+            "size": 111646,
+            "uid": "3c1b8fbe59af4dd29eeb8b4b899c5faa",
+            "height": 576
+        }, {
+            "url": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/urls\/67d4415608c1474eac0298bfda83a079\/dist\/thumbnails\/29796177d94c4f03b5b189515a14233c\/720x405.jpeg",
+            "width": 720,
+            "size": 58024,
+            "uid": "402876dbe69a4f3f94ee4dacf04700d9",
+            "height": 405
+        }, {
+            "url": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/urls\/67d4415608c1474eac0298bfda83a079\/dist\/thumbnails\/29796177d94c4f03b5b189515a14233c\/256x144.jpeg",
+            "width": 256,
+            "size": 9702,
+            "uid": "3295448618a14cf88c8810873b40b42b",
+            "height": 144
+        }, {
+            "url": "https:\/\/dg5bepmjyhz9h.cloudfront.net\/urls\/67d4415608c1474eac0298bfda83a079\/dist\/thumbnails\/29796177d94c4f03b5b189515a14233c\/64x36.jpeg",
+            "width": 64,
+            "size": 1255,
+            "uid": "0526f871b15e452d860ebb355b7e1de6",
+            "height": 36
+        }], "uid": "29796177d94c4f03b5b189515a14233c"
+    }
+    ,
+    "animationCount": 0,
+    "brandedAt": null,
+    "name": "Chair",
+    "license": null,
+    "editorUrl": "https:\/\/sketchfab.com\/models\/67d4415608c1474eac0298bfda83a079\/edit",
+    "isPublished": false,
+    "createdAt": "2017-02-06T20:51:46.839507",
+    "faceCount": 257305,
+    "ext": "obj",
+    "downloadCount": 0,
+    "options": {
+        "version": 2, "orientation": {
+            "matrix": [1, 0, 0, 0, 0, 0.0, -1, 0, 0, 1, 0.0, 0, 0, 0.1875025071, 0.1574469898, 1]
+        }
+        ,
+        "camera": {
+            "position": [-0.0941543241, -0.7735535581, 0.440268858],
+            "target": [1.5802156828, -0.32944201, -0.7284082499]
+        }
+        ,
+        "wireframe": {
+            "color": "000000FF", "enable": false
+        }
+        ,
+        "scene": {
+            "fov": 45, "postProcess": {
+                "toneMapping": {
+                    "saturation": 1, "enable": false, "brightness": 0, "method": "default", "contrast": 0, "exposure": 1
+                }
+                ,
+                "enable": true, "dof": {
+                    "focusPoint": [1.1555997868, -0.4235278801, -0.634601902],
+                    "blurFar": 0.5,
+                    "enable": true,
+                    "blurNear": 0.5
+                }
+                ,
+                "vignette": {
+                    "lens": [0.8, 0.25], "enable": true
+                }
+                ,
+                "grain": {
+                    "enable": true, "animated": true, "factor": 0.2044198895
+                }
+                ,
+                "chromaticAberration": {
+                    "enable": false, "factor": 0.05
+                }
+                ,
+                "sharpen": {
+                    "enable": true, "factor": 0.2
+                }
+                ,
+                "colorBalance": {
+                    "high": [0, 0, 0], "enable": false, "low": [0, 0, 0], "mid": [0, 0, 0]
+                }
+                ,
+                "bloom": {
+                    "threshold": 0, "enable": false, "radius": 0.7, "factor": 0.5
+                }
+            }
+        }
+        ,
+        "hotspot": {
+            "hotspots": []
+        }
+        ,
+        "environment": {
+            "backgroundExposure": 1,
+            "enable": true,
+            "uid": "16cdcddf277d423d99094f8db0c731e9",
+            "blur": 0.1,
+            "rotation": 0,
+            "exposure": 2.0569
+        }
+        ,
+        "vr": {
+            "floorHeight": -1.5979000604, "scale": 1, "initialCamera": {
+                "position": [-0.0392149878, -0.5747473939, -1.5979000604],
+                "rotation": [0, 0, -0.5751056459, 0.8180791502]
+            }
+        }
+        ,
+        "materials": {
+            "5bfe21ab-8f49-489b-8596-ac02e1fe47b8": {
+                "channels": {
+                    "Opacity": {
+                        "color": [1, 1, 1], "invert": false, "enable": false, "type": "alphaBlend", "factor": 1
+                    }
+                    ,
+                    "DiffuseColor": {
+                        "enable": true, "texture": {
+                            "wrapS": "REPEAT",
+                            "minFilter": "LINEAR_MIPMAP_LINEAR",
+                            "uid": "be80b4b23b4d453fa89c5778f6f1c2b9",
+                            "textureTarget": "TEXTURE_2D",
+                            "magFilter": "LINEAR",
+                            "texCoordUnit": 0,
+                            "internalFormat": "RGB",
+                            "wrapT": "REPEAT"
+                        }
+                        ,
+                        "factor": 1
+                    }
+                    ,
+                    "AlbedoPBR": {
+                        "enable": true, "texture": {
+                            "wrapS": "REPEAT",
+                            "minFilter": "LINEAR_MIPMAP_LINEAR",
+                            "uid": "be80b4b23b4d453fa89c5778f6f1c2b9",
+                            "textureTarget": "TEXTURE_2D",
+                            "magFilter": "LINEAR",
+                            "texCoordUnit": 0,
+                            "internalFormat": "RGB",
+                            "wrapT": "REPEAT"
+                        }
+                        ,
+                        "factor": 1
+                    }
+                    ,
+                    "EmitColor": {
+                        "color": [0.2, 0.2, 0.2], "enable": false, "type": "additive", "factor": 0
+                    }
+                    ,
+                    "Displacement": {
+                        "color": [1, 1, 1], "enable": false, "factor": 0
+                    }
+                    ,
+                    "AOPBR": {
+                        "color": [1, 1, 1], "occludeSpecular": false, "enable": false, "factor": 1
+                    }
+                    ,
+                    "DiffusePBR": {
+                        "enable": false, "texture": {
+                            "wrapS": "REPEAT",
+                            "minFilter": "LINEAR_MIPMAP_LINEAR",
+                            "uid": "be80b4b23b4d453fa89c5778f6f1c2b9",
+                            "textureTarget": "TEXTURE_2D",
+                            "magFilter": "LINEAR",
+                            "texCoordUnit": 0,
+                            "internalFormat": "RGB",
+                            "wrapT": "REPEAT"
+                        }
+                        ,
+                        "factor": 1
+                    }
+                    ,
+                    "SpecularColor": {
+                        "color": [1, 1, 1], "enable": true, "factor": 0
+                    }
+                    ,
+                    "RoughnessPBR": {
+                        "color": [1, 1, 1], "enable": true, "factor": 0.6
+                    }
+                    ,
+                    "DiffuseIntensity": {
+                        "color": [1, 1, 1], "enable": true, "factor": 1
+                    }
+                    ,
+                    "GlossinessPBR": {
+                        "color": [1, 1, 1], "enable": false, "factor": 0.4
+                    }
+                    ,
+                    "NormalMap": {
+                        "color": [1, 1, 1], "flipY": true, "enable": false, "factor": 1
+                    }
+                    ,
+                    "SpecularHardness": {
+                        "color": [1, 1, 1], "enable": true, "factor": 0
+                    }
+                    ,
+                    "BumpMap": {
+                        "color": [1, 1, 1], "enable": false, "factor": 1
+                    }
+                    ,
+                    "CavityPBR": {
+                        "color": [1, 1, 1], "enable": false, "factor": 1
+                    }
+                    ,
+                    "MetalnessPBR": {
+                        "color": [1, 1, 1], "enable": true, "factor": 0
+                    }
+                    ,
+                    "SpecularPBR": {
+                        "color": [1, 1, 1], "enable": false, "factor": 0.05
+                    }
+                    ,
+                    "SpecularF0": {
+                        "color": [1, 1, 1], "enable": true, "factor": 0.5
+                    }
+                }
+                ,
+                "name": "material_0",
+                "shadeless": false,
+                "stateSetID": 1,
+                "version": 3,
+                "texCoordUnitList": [0],
+                "cullFace": "BACK",
+                "id": "5bfe21ab-8f49-489b-8596-ac02e1fe47b8",
+                "reflection": 0.1
+            }
+            ,
+            "updatedAt": 14864146775
+        }
+        ,
+        "lighting": {
+            "lights": [{
+                "enable": true,
+                "angle": 45,
+                "matrix": [-0.98158045, 0.1909922167, -0.0046683273, 0, 0.135483471, 0.7131126062, 0.6878332937, 0, 0.1346998486, 0.6745312328, -0.7258536814, 0, 1.6653776481, 1.4433929213, -1.3826451345, 1],
+                "color": [0.7806122449, 0.7985214494, 1],
+                "intensityGround": 1,
+                "attachedToCamera": true,
+                "falloff": 0.7042209752,
+                "castShadows": true,
+                "intensity": 0.96,
+                "shadowBias": 0.005,
+                "ground": [0.3, 0.2, 0.2],
+                "type": "DIRECTION",
+                "hardness": 0.5,
+                "shadowSize": 1024
+            }, {
+                "enable": true,
+                "angle": 45,
+                "matrix": [0.7755211495, -0.066660286, 0.6277924442, 0, 0.2690483737, 0.9344846523, -0.2331338825, 0, -0.5711216326, 0.3497067927, 0.7426474533, 0, 0.3355039467, 0.8313748799, 1.3842316064, 1],
+                "color": [1, 0.878331945, 0.7091836735],
+                "intensityGround": 1,
+                "attachedToCamera": true,
+                "falloff": 0.7042209752,
+                "castShadows": true,
+                "intensity": 1,
+                "shadowBias": 0.005,
+                "ground": [0.3, 0.2, 0.2],
+                "type": "DIRECTION",
+                "hardness": 0.5,
+                "shadowSize": 1024
+            }, {
+                "enable": true,
+                "angle": 45,
+                "matrix": [0.6903299131, -0.7076363669, -0.1506498699, 0, -0.0110896982, 0.197851373, -0.980169298, 0, 0.7234097246, 0.6783108479, 0.1287352474, 0, 2.7745954925, 1.450514284, 0.2275287864, 1],
+                "color": [0.4234693878, 0.7352665556, 1],
+                "intensityGround": 0.06,
+                "attachedToCamera": false,
+                "falloff": 0.7042209752,
+                "castShadows": true,
+                "intensity": 0.12,
+                "shadowBias": 0.005,
+                "ground": [0.9642857143, 0.6484128637, 0.2607507289],
+                "type": "HEMI",
+                "hardness": 0.5,
+                "shadowSize": 1024
+            }], "enable": false
+        }
+        ,
+        "background": {
+            "color": [0.2, 0.2, 0.2],
+            "enableVR": false,
+            "enable": "environment",
+            "uid": "51af6a870cce449eb75b0345feebaebb"
+        }
+        ,
+        "updatedAt": "2017-02-06T20:59:39.690770", "shading": {
+            "vertexColor": {
+                "enable": false, "colorSpace": "srgb", "useAlpha": false
+            }
+            ,
+            "type": "shadeless", "pointSize": 2, "renderer": "pbr"
+        }
+        ,
+        "animation": {
+            "speed": 1, "order": [], "cycleMode": "one", "autoplay": false
+        }
+        ,
+        "createdAt": "2017-02-06T20:51:46.850725"
+    }
+}
